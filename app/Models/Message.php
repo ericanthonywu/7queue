5190c637630bee40c1d5b12899db221bfab66df4
Eric Anthony <ericanthonywu89@gmail.com>
2019-07-12 21:24:14 +0700

    asd
    Signed-off-by: Eric Anthony <ericanthonywu89@gmail.com>

---
+++ app/Models/Message.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Fri, 12 Jul 2019 14:02:14 +0700.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class Message
+ *
+ * @property int $id
+ * @property int $customer
+ * @property string $judul
+ * @property string $pesan
+ * @property int $push_notif
+ * @property int $tipe
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @package App\Models
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereCustomer($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereJudul($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message wherePesan($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message wherePushNotif($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereTipe($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Message extends Eloquent
+{
+    protected $table = 'message';
+    protected $casts = [
+        'customer' => 'int',
+        'push_notif' => 'int',
+        'tipe' => 'int'
+    ];
+    protected $fillable = [
+        'customer',
+        'judul',
+        'pesan',
+        'push_notif',
+        'tipe'
+    ];
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d');
+    }
+}
---
+++ database/migrations/2019_07_12_135844_create_message.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('customer');
+            $table->mediumText('judul');
+            $table->mediumText('pesan');
+            $table->unsignedTinyInteger('push_notif');
+            $table->unsignedTinyInteger('tipe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
